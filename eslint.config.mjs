86d3d94495558a11fbc0c02d8c1cac79b5fa3dbd
Olivier Van den Mooter <olivier.vandenmooter@digipolis.be>
2026-09-21 13:38:37 +0200

    update/reduce dep

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,4 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-
 
 export default [
     {
@@ -10,8 +8,8 @@ export default [
     {
         languageOptions: {
             globals: {
-                ...globals.browser,
-                ...globals.node
+                "console": false,
+                "Buffer": false,
             }
         },
         rules: {
